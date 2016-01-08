19022caa8171b3fbc56cedd92b46db2782efbc86
DOG <DOG@DOGs-Mac.local>
2016-01-08 10:33:13 -0800

    Initial Commit

---
+++ I Remember/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Remember
+//
+//  Created by DOG on 1/8/16.
+//  Copyright (c) 2016 DOG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
